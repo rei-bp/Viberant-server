d08bac8b5180f3134b557dd1b3bcb66238c415ca
Consecratio <jcasique@gmail.com>
2021-07-07 16:35:29 -0700

    added hashing of user password

---
+++ controllers/api-v1/users.js
@@ -0,0 +1,58 @@
+const router = require('express').Router()
+const db = require('../../models')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+
+// GET /users -- test api endpoint
+router.get('/', (req, res) => {
+    res.json({ msg: 'router is working' })
+})
+
+// POST /users/register -- CREATE a new user (aka registration)
+router.post('/register', async (req, res) => {
+    try {
+        // check if the user exists already
+        const findUser = await db.User.findOne({
+            email: req.body.email
+        })
+
+        // if the user is found -- don't let them register
+        if(findUser) return res.status(400).json({ msg: 'user already exists in the db' })
+        console.log(findUser)
+
+        // hash password from req.body
+        const password = req.body.password
+        const salt = 12
+        const hashedPassword = await bcrypt.hash(password, salt)
+
+        // create our new user
+        const newUser = db.User({
+            name: req.body.name,
+            email: req.body.email,
+            password: hashedPassword
+        })
+
+        await newUser.save()
+
+        // create the jwt payload
+        const payload = {
+            name: newUser.name,
+            email: newUser.email,
+            id: newUser.id
+        }
+
+        // sign the jwt and send a response
+        const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: 60 * 60 })
+
+        res.json({ token })
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({ msg: 'internal server error' })
+    }
+})
+
+// POST /users/login -- validate login credentials
+
+// GET /auth-locked -- will redirect if a bad jwt is found
+
+module.exports = router
--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const express = require('express')
 const cors = require('cors')
 const rowdy = require('rowdy-logger')
 
+// connect to db
+const db = require('./models')
+db.connect()
+
 // configure express app
 const app = express()
 const PORT = process.env.PORT || 3001
@@ -16,6 +20,9 @@ app.use(cors())
 // body parser middleware
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json()) // for the request body
+
+// controllers
+app.use('/api-v1/users', require('./controllers/api-v1/users.js'))
 
 // routes
 app.get('/', (req, res) => {
