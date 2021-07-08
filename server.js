b62b9cbbf3d5f571907714160e5a9a84870e8dcd
Consecratio <jcasique@gmail.com>
2021-07-08 09:47:02 -0700

    finished auth lesson

--- server.js
+++ server.js
@@ -15,17 +15,29 @@ const app = express()
 const PORT = process.env.PORT || 3001
 const rowdyResults = rowdy.begin(app)
 
-// middlewares
+// middlewares - middleware order MATTERS
 app.use(cors())
 // body parser middleware
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json()) // for the request body
+// custom middleware
+app.use((req, res, next) => {
+    console.log(`income request on: ${req.method} ${req.url}`)
+    res.locals.anything = `🚀`
+    next()
+})
 
 // controllers
 app.use('/api-v1/users', require('./controllers/api-v1/users.js'))
 
+const middleWare = (req, res, next) => {
+    console.log('i am a route specific middleware! 👾')
+    next()
+}
+
 // routes
-app.get('/', (req, res) => {
+app.get('/', middleWare, (req, res) => {
+    console.log(res.locals)
     res.json({ msg: "Hello from our backend!" })
 })
 
